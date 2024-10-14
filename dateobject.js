d18fe731194168b4c3e7b396f117bb82efa49b52
chelz2 <golirida@hotmail.com>
2024-10-15 02:33:59 +0530

    date object in JS

---
+++ dateobject.js
@@ -0,0 +1,27 @@
+let x;
+
+let d = new Date();
+
+x = d.toDateString();
+x = d.getTime();
+x = d.valueOf();
+x = d.getFullYear();
+x = d.getDay();
+x = d.getDate();
+x = d.getMonth();
+x = d.getHours();
+
+x = `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
+
+// modern way to retrieve & represent date object data
+x = Intl.DateTimeFormat("en-US").format();
+x = Intl.DateTimeFormat("default", { month: "long" }).format();
+// or :-
+x = d.toLocaleDateString("default", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
+
+console.log(x);
