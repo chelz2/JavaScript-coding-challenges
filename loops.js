a1204940241c75211e98525054ef9cc1b045ed4d
chelz2 <golirida@hotmail.com>
2024-10-19 18:51:12 +0530

    loops in js - FizzBuzz challenge

---
+++ loops.js
@@ -0,0 +1,32 @@
+// loops - FizzBuzz challenge
+
+// for count 1-100- print fizz for multiple of 3, buzz for multiple of 5, & FizzBuzz for multiple of both 5 & 3
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// using while loop
+
+let i = 1;
+
+while (i <= 100) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+  i++;
+}
