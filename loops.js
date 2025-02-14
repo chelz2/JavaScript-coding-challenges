e062669a28d3d7a3f3f0807435bf44d8e3b5dae7
chelz2 <golirida@hotmail.com>
2025-02-15 00:54:15 +0530

    updating

--- loops.js
+++ loops.js
@@ -16,9 +16,9 @@ for (let i = 1; i <= 100; i++) {
 
 // using while loop
 
-let i = 101;
+let i = 1;
 
-while (i <= 200) {
+while (i <= 100) {
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
   } else if (i % 3 === 0) {
