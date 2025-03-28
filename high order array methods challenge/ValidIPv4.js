eda9a0b9b45afe2e91653b9d6957532d56a752c5
chelz2 <golirida@hotmail.com>
2025-03-28 21:10:15 +0530

    liitle tweak / correction - check for edges cases if pass in char string

--- high order array methods challenge/ValidIPv4.js
+++ high order array methods challenge/ValidIPv4.js
@@ -5,34 +5,16 @@ function isValidIpv4(str) {
   }
   const arr = str.split(".");
 
-  const bool = arr.every((octat) => {
-    return typeof +octat === "number" && +octat === "NaN";
+  if (arr.length > 4) {
+    return false;
+  }
+
+  return arr.every((octet) => {
+    const num = parseInt(octet);
+    return num >= 0 && num <= 255 && octet === num.toString();
   });
-
-  if (bool === false) {
-    console.log(false);
-    return false;
-  }
-
-  if (
-    bool === false ||
-    arr.length === 0 ||
-    arr.length !== 4 ||
-    +arr[0] < 0 ||
-    +arr[0] > 255 ||
-    +arr[1] < 0 ||
-    +arr[1] > 255 ||
-    +arr[2] < 0 ||
-    +arr[2] > 255 ||
-    +arr[3] < 0 ||
-    +arr[3] > 255
-  ) {
-    console.log(false);
-    return false;
-  }
-
-  console.log(true);
-  return true;
 }
 
-isValidIpv4("33.33.33.33");
+const ipv4 = isValidIpv4("3s.33.33.33");
+
+console.log(ipv4);
