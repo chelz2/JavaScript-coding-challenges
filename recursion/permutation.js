3893dcfb85d5edfab941e49897015719d0c6beac
chelz2 <golirida@hotmail.com>
2025-04-05 11:46:52 +0530

    permutation problems - using recursion + loop

--- recursion/permutation.js
+++ recursion/permutation.js
@@ -1,8 +1,24 @@
+// permutation using loops & recursion combination
+
 function permutation(str) {
   let result = [];
   // base case
-  if (str.length === 0) {
+  if (str === "") {
     result.push("");
     return result;
   }
+
+  for (let i = 0; i < str.length; i++) {
+    const firstChar = str[i];
+    const restOfChar = str.slice(0, i) + str.slice(i + 1);
+    const subPermutation = permutation(restOfChar);
+
+    for (let j = 0; j < subPermutation.length; j++) {
+      result.push(firstChar + subPermutation[j]);
+    }
+  }
+
+  return result;
 }
+
+console.log(permutation("car")); // -> [ 'car', 'cra', 'acr', 'arc', 'rca', 'rac' ]
